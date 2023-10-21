275c4e01de782da9d6cd2ad573a8afa3f3dc41e5
sholohov <sholohov@users.noreply.github.com>
2023-10-21 15:45:22 +0000

    deploy: sholohov/blitz-commander@dcbddc74f81e310056785adcd036e1b1a514baea

---
+++ _nuxt/43dbcd9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{580:function(n,o,r){"use strict";var t=r(2),c=r(55).find,d=r(182),e="find",f=!0;e in[]&&Array(1)[e]((function(){f=!1})),t({target:"Array",proto:!0,forced:f},{find:function(n){return c(this,n,arguments.length>1?arguments[1]:void 0)}}),d(e)}}]);
