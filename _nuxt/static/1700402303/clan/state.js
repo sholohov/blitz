13f6085f8572699d392f4ec1d8a8334961ab5c3c
sholohov <sholohov@users.noreply.github.com>
2023-11-19 14:12:16 +0000

    deploy: sholohov/blitz-commander@bc83259e30f791eaa625a09cf50aee0c135263dc

--- _nuxt/static/1700402303/clan/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700402303",layout:"default",error:a,serverRendered:false,routePath:"\u002Fclan",config:{clientVersion:"0.25.1",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
