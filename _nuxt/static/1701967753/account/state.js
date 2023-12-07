d831d11385be6935eb41d49f5eaf886369184a51
sholohov <sholohov@users.noreply.github.com>
2023-12-07 17:57:08 +0000

    deploy: sholohov/blitz-commander@3296cc5575e9ff5d817acebd27c23282178c9691

--- _nuxt/static/1701967753/account/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701967753",layout:"default",error:a,serverRendered:false,routePath:"\u002Faccount",config:{clientVersion:"0.28.1",_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
