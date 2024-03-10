4f19992e28888e3a0f40dbadeab0044229b94747
sholohov <sholohov@users.noreply.github.com>
2024-03-10 15:18:46 +0000

    deploy: sholohov/blitz-commander@1f00bc4c999327c02d74355926318f3bdef4b399

---
+++ _nuxt/index.KZvLsDFS.js
@@ -0,0 +1,1 @@
+import{f as e,c as n,o}from"./entry.BCxoBdpj.js";const s=e({__name:"index",setup(t){return(a,c)=>(o(),n("div",null," accounts index page "))}});export{s as default};
---
+++ _nuxt/user.BXGQUIUO.js
@@ -0,0 +1,1 @@
+import{as as s}from"./entry.BCxoBdpj.js";const t={accountId:0,name:"Гость"},o=s("auth",{state:()=>({...t}),actions:{resetUser(){this.$patch({...t})},setUser(e){this.$patch(e)}}});export{o as u};
