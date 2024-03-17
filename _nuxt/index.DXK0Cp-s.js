c36f65b782cbe2889ee1ef766f1ee749d0f46329
sholohov <sholohov@users.noreply.github.com>
2024-03-17 16:46:21 +0000

    deploy: sholohov/blitz-commander@d2340c2d276c38c957d0963c9d846237b1225456

--- _nuxt/index.DXK0Cp-s.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./entry.BwCe-mLo.js";const t=e({__name:"index",setup(n){return()=>{}}});export{t as default};
--- _nuxt/user.CMYeEeya.js
+++
@@ -1,1 +0,0 @@
-import{as as s}from"./entry.BwCe-mLo.js";const t={accountId:0,name:"Гость"},o=s("auth",{state:()=>({...t}),actions:{resetUser(){this.$patch({...t})},setUser(e){this.$patch(e)}}});export{o as u};
