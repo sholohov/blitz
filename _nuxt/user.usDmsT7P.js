fa4f21846fcc04c02f3aafadd8d060b95050c42b
sholohov <sholohov@users.noreply.github.com>
2024-04-01 05:30:22 +0000

    deploy: sholohov/blitz-commander@dd6ed5b91908776266bfd09560dcff2f87f6e367

---
+++ _nuxt/user.usDmsT7P.js
@@ -0,0 +1,1 @@
+import{at as s}from"./entry.r0roUpCv.js";const t={accountId:0,name:"Гость"},o=s("auth",{state:()=>({...t}),actions:{resetUser(){this.$patch({...t})},setUser(e){this.$patch(e)}}});export{o as u};
