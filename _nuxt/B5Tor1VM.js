d7b2559f23fcdab91366193d775fb1fdfef34ca0
sholohov <sholohov@users.noreply.github.com>
2024-06-02 16:16:45 +0000

    deploy: sholohov/blitz-commander@36f20d88d5d29372acd1494323f003c31597d874

--- _nuxt/B5Tor1VM.js
+++
@@ -1,1 +0,0 @@
-import{au as s}from"./m8Dh55rh.js";const t={accountId:0,name:"Гость"},o=s("auth",{state:()=>({...t}),actions:{resetUser(){this.$patch({...t})},setUser(e){this.$patch(e)}}});export{o as u};
--- _nuxt/LDSQCNDN.js
+++
@@ -1,1 +0,0 @@
-import{f as e}from"./m8Dh55rh.js";const t=e({__name:"index",setup(n){return()=>{}}});export{t as default};
